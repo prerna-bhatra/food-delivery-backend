e325930a55c3d9af1fd27385231577a50f68a6d3
prerna <prerna@onelabventures.com>
2024-05-02 18:07:03 +0530

    remote db

--- db/index.js
+++ db/index.js
@@ -1,6 +1,29 @@
 const { Sequelize, DataTypes } = require('sequelize');
 
-const sequelize = new Sequelize(process.env.DBURL);
+// const sequelize = new Sequelize(process.env.DBURLDEV);
+
+// const sequelize = new Sequelize('postgres-food', 'postgres', 'PravyaBhatra1998', {
+//     host: 'postgres-food.c1gi0mumgho5.ap-south-1.rds.amazonaws.com',
+//     dialect: 'postgres',
+//     port: 5432,
+//     logging: false // Disable logging SQL queries (optional)
+//   });
+
+
+const sequelize = new Sequelize(
+    'food-app',
+    'postgres',
+    'PravyaBhatra1998',
+    {
+      host:  'postgres-food.c1gi0mumgho5.ap-south-1.rds.amazonaws.com',
+      dialect:  'postgres',
+      dialectOptions: {
+        ssl: {
+          require: true, // This will help you. But you will see nwe error
+          rejectUnauthorized: false // This line will fix new error
+        }
+      },    }
+  );
 
 module.exports = {
     sequelize
