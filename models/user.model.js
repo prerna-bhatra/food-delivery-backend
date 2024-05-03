c3d80b37a50ae0dc7cacf0bb2108f69d2259f791
prerna <prerna@onelabventures.com>
2024-05-03 15:48:37 +0530

    usernam fix

--- models/user.model.js
+++ models/user.model.js
@@ -4,9 +4,7 @@ const {sequelize} = require('../db/index.js');
 const User = sequelize.define('User', {
     username: {
         type: DataTypes.STRING,
-        allowNull: false,
-        unique: true
-    },
+        allowNull: false    },
     email: {
         type: DataTypes.STRING,
         allowNull: false,
